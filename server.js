cc8b5258b2aadbee87141b5e919dd6da1978f8e6
davcarlos <davcarlos@gmail.com>
2021-09-08 16:27:17 -0500

    my first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const methodOverride = require('method-override');
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+});
+
+app.set('view engine','ejs');
+
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+
+
+app.get('/', async (req,res)=> {
+
+   const articles =  await Article.find();
+
+    
+    res.render('articles/index', {articles: articles});
+});
+
+app.use('/articles',articleRouter);
+
+app.listen(5000);
